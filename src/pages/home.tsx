0511729fe275b8f1e747c68aee16b683d70a457f
김기만 <rlarlaks21c@naver.com>
2021-11-16 10:31:03 +0900

    분유 시작 시간 추가

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
 
 const Home: React.FC = () => {
   const birthday = new Date(2021, 6, 23);
@@ -7,15 +8,67 @@ const Home: React.FC = () => {
 
   const dPlusDay = Math.floor(plusTime / (1000 * 60 * 60 * 24)) + 1;
   const dPlusWeek = Math.floor(plusTime / (1000 * 60 * 60 * 24 * 7));
+  const dPlusMonth = Math.floor(plusTime / (1000 * 60 * 60 * 24 * 30));
   const dPlusDayOfWeek = Math.floor(
     (plusTime % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24)
   );
 
+  const milkString = localStorage.getItem("milk");
+  const [milkList, setMilkList] = useState<Array<string>>(
+    JSON.parse(milkString || "[]")
+  );
+
+  const addMilk = () => {
+    const newMilkList = [new Date().toISOString(), ...milkList];
+    setMilk(newMilkList);
+  };
+
+  const resetMilk = () => {
+    const newMilkList: Array<string> = [];
+    setMilk(newMilkList);
+  };
+
+  const setMilk = (newMilkList: Array<string>) => {
+    setMilkList(newMilkList);
+    localStorage.setItem("milk", JSON.stringify(newMilkList));
+  };
+
   return (
-    <div>
-      <h2 style={{ color: "cadetblue" }}>
-        아영이 태어난지 {dPlusDay}일, {dPlusWeek}주{dPlusDayOfWeek}일
-      </h2>
+    <div style={{ color: "cadetblue" }}>
+      <h2 style={{ marginBottom: 0 }}>아영이 태어난지 {dPlusDay}일</h2>
+      <div>
+        ({dPlusWeek}주{dPlusDayOfWeek}일, {dPlusMonth}개월)
+      </div>
+      <div>
+        <h3 style={{ float: "left", marginLeft: 15 }}>* 분유</h3>
+        <div style={{ float: "right", marginRight: 15, marginTop: 18 }}>
+          <button
+            type="button"
+            style={{ width: 100 }}
+            onClick={() => {
+              addMilk();
+            }}
+          >
+            시작
+          </button>
+          <button
+            type="button"
+            style={{ marginLeft: 10, width: 100 }}
+            onClick={() => {
+              resetMilk();
+            }}
+          >
+            리셋
+          </button>
+        </div>
+      </div>
+      <div style={{ marginTop: 70 }}>
+        <ul>
+          {milkList.map((milk) => (
+            <li>{moment(milk).format("YYYY-MM-DD HH:mm")}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
